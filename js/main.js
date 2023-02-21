1756c85a942af53cba91fc5dbf2da38da7228c2a
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-02-21 13:51:56 +0100

    Esercizio completato

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const myName = prompt("Dimmi il tuo nome");
+
+const myLastName = prompt("Dimmi il tuo cognome");
+
+const myFavColor = prompt("Dimmi il tuo colore preferito");
+
+document.getElementById("password").innerHTML = myName + myLastName + myFavColor + 23;
